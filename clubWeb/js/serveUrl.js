fb63e81e8f08a793982f108a84788d9c834a7310
江祥盛 <29163916@qq.com>
2024-02-07 12:43:16 +0800

    基于Vue+PHP的社团面试信息管理系统

---
+++ clubWeb/js/serveUrl.js
@@ -0,0 +1,2 @@
+/** 在‘’内输入服务器根地址，地址最后不包括‘/’ **/
+let serveBaseURL = 'https://468813y2a2.goho.co/server'//服务器地址
---
+++ server/src/conn.php
@@ -0,0 +1,1 @@
+<?php  require_once '../config.php';$systemAdminQq = '2916391636'; $conn = new mysqli($servername, $username, $password, $dbname); if ($conn->connect_error) { die("连接失败：" . $conn->connect_error);}  ?>
